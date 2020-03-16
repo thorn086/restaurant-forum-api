ad20d867841e22bb240374ddce4eb886edebcd4d
Darren Rose <thorn086@gmail.com>
2020-03-16 08:58:14 -0500

    updated client origin

--- src/app.js
+++ src/app.js
@@ -20,8 +20,10 @@ const morganOption = (NODE_ENV === 'production'
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
- //{origin: CLIENT_ORIGIN}
+app.use(cors(
+   {origin: CLIENT_ORIGIN}
+))
+
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
