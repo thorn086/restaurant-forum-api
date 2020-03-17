89a3b937685236a3c2b266811da570ee6a0d19cd
Darren Rose <thorn086@gmail.com>
2020-03-16 19:47:09 -0500

    added auth

--- src/app.js
+++ src/app.js
@@ -20,8 +20,14 @@ const morganOption = (NODE_ENV === 'production'
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
-  //{origin: CLIENT_ORIGIN}
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.use(cors(
+   {origin: CLIENT_ORIGIN}
+))
+ 
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
